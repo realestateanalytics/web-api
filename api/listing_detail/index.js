4835d96fe02340041932ccaa4f273741415baead
Konstantin Zabashta <kosta.zabashta@gmail.com>
2016-07-27 12:05:40 -0400

    Added the listing detail endpoint

--- api/listing_detail/index.js
+++ api/listing_detail/index.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var Q = require("q");
+var Q = require('q');
 var _ = require('lodash');
 
-router.get('/', function(req, res) {
-  return res.status(200).json([]);
+var ListingDetail = require('./listing_detail.model')
+
+router.get('/:mls_id', function(req, res) {
+  var mls_id = req.params.mls_id;
+
+  ListingDetail.findOne({mls_id: mls_id}, function(err, listing){
+    return res.status(200).json(listing);
+  });
 });
 
 module.exports = router;
