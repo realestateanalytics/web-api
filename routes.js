a07af7b717573ee7857a92a3620f4202aadcaf19
Konstantin Zabashta <kosta.zabashta@gmail.com>
2016-07-28 16:35:20 -0400

    Allow for CORS

--- routes.js
+++ routes.js
@@ -3,5 +3,10 @@
 var path = require('path');
 
 module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
   app.use('/api/listing_detail', require('./api/listing_detail'));
 };
